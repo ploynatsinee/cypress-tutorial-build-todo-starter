6ea37a443fb5737c87917728316c30487660fe24
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-09-23 12:53:29 +0700

    remove node loader

--- cypress.config.js
+++ cypress.config.js
@@ -13,6 +13,7 @@ module.exports = defineConfig({
   module: {
     rules: [{ test: /\.txt$/, use: 'raw-loader' }],
   },
+  mode: 'production',
 });
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,11 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'], exclude: /node_modules/ }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'], exclude: /node_modules/ },
+      
     ]
   },
+  
   devtool: 'source-map',
   plugins: [HtmlWebpackPluginConfig]
  }
