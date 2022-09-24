57c2a1844bf218012602be4286f0593696e8f54d
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-09-24 14:40:05 +0700

    I have no idea

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,19 @@
 const { defineConfig } = require("cypress");
-const path = require('path');
+const path = require("path");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      baseUrl = 'http://localhost:3030'
-    },
+    // setupNodeEvents(on, config) {
+      // baseUrl = "http://localhost:3030";
+    // },
+    baseUrl: "http://localhost:3030",
   },
   output: {
-    filename: 'app.bundle.js',
+    filename: "app.bundle.js",
   },
   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+    rules: [{ test: /\.txt$/, use: "raw-loader" }],
   },
-  mode: 'production',
+  mode: "production",
 });
-
-
--- cypress/e2e/input-form.cy.js
+++ cypress/e2e/input-form.cy.js
@@ -1,29 +1,23 @@
-describe('input form', () => {
+describe("input form", () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit("/");
+  });
 
-  it('focuses input on load', () => {
+  it("focuses input on load", () => {
+    cy.focused().should("have.class", "new-todo");
+  });
 
-    cy.focused()
-      .should('have.class', 'new-todo')
-  })
+  it("accept input", () => {
+    const typedText = "Buy Milk";
+    cy.get(".new-todo").type(typedText).should("have.value", typedText);
+  });
 
-it('accept input', () => {
-  const typedText = 'Buy Milk'
-
-
-  cy.get('.new-todo')
-     .type(typedText)
-     .should('have.value', typedText)
-})
-
-  context('From submission', () => {
-    it.only('Add a new todo on submit', () => {
-      cy.get('.new-todo')
-        .type('Buy eggs')
-        .type('{enter}')
+  context("From submission", () => {
+    Cypress.on('uncaught:exception', (err) => {
+      it.only("Add a new todo on submit", () => {
+      cy.get(".new-todo").type("Buy eggs").type("{enter}");
+    });
     })
-  })
-
-})
+    
+  });
+});
