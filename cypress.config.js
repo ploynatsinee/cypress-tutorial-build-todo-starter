a0b39e3717781beffcfa15bda898762abb8ab5ec
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-09-29 12:15:39 +0700

    update

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,7 @@ const path = require("path");
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   baseUrl: "http://localhost:3030";
-    // },
-    baseUrl: "http://localhost:3030",
+    baseUrl: "http://web:3080",
   },
   output: {
     filename: "app.bundle.js",
--- src/lib/service.js
+++ src/lib/service.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export const saveTodo = (todo) => 
-    axios.post('http://localhost:3030/api/todos', todo)
+    axios.post('http://web:3080/api/todos', todo)
 
 export const loadTodos = () =>
-    axios.get('http:///localhost:3030/api/todos')
+    axios.get('http:///web:3080/api/todos')
 
 export const destroyTodo = (id) =>
-    axios.delete(`http:///localhost:3030/api/todos/${id}`)
+    axios.delete(`http:///web:3080/api/todos/${id}`)
 
 export const updateTodo = (todo) =>
-    axios.put(`http://localhost:3030/api/todos/${todo.id}`, todo)
+    axios.put(`http://web:3080/api/todos/${todo.id}`, todo)
